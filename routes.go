a0fcdab03fb73d0f243513a550248fc73583c55c
maxsupermanhd <q3.max.2011@ya.ru>
2026-09-10 23:05:49 +0300

    improve testpage

--- routes.go
+++ routes.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	"main/frontend"
+	"math/rand/v2"
 	"net/http"
+	"sync"
+	"sync/atomic"
 
 	"github.com/a-h/templ"
 	"golang.org/x/net/websocket"
@@ -24,7 +27,31 @@ func makeHTTPServeMux() http.HandlerFunc {
 	return mux.ServeHTTP
 }
 
-func serveTestpage(w http.ResponseWriter, r *http.Request) {
+var (
+	testpageBufs = sync.Pool{
+		New: func() any {
+			return &bytes.Buffer{}
+		},
+	}
+	testpageSeed atomic.Uint64
+)
+
+func serveTestpage(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(200)
-	w.Write(bytes.Repeat([]byte{'f'}, 500_000))
+	rngSeed := testpageSeed.Add(2)
+	rng := rand.NewPCG(uint64(rngSeed), uint64(rngSeed+1))
+	buf := testpageBufs.Get().(*bytes.Buffer)
+	i := 0
+	for i < 200_000 {
+		v := rng.Uint64()
+		i += 64 / 4
+		for range 64 / 4 {
+			c := 0x20 + v&0x4f
+			v >>= 4
+			buf.WriteByte(byte(c))
+		}
+	}
+	w.Write(buf.Bytes())
+	buf.Reset()
+	testpageBufs.Put(buf)
 }
